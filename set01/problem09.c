58e0ba5ae0c5f9f5eae0b07022d13c8c99f3712e
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2022-12-12 06:57:21 +0000

    solved

--- set01/problem09.c
+++ set01/problem09.c
@@ -1,28 +1,43 @@
-#include <math.h>
 #include <stdio.h>
-float input();
-float square_root(float n);
-void output(float n, float sqrroot);
+#structure complex
+{
+  int real,img;
+}
+typedef structure complex;
+int get_n();
+complex input_complex();
+void input_n_complex(int n,complex C[n]);
+complex add (complex a, complex b);
+complex add_n_complex(int n, complex c[n]);
+void output(int n, complex c[n],complex result);
+
 int main()
 {
-  double n ,sqrroot;
-  n=input();
-  sqrroot=square_root(n);
-  output(n, sqrroot);
-}  
-float input()
-{
-  double n;
-  printf("Enter the number ");
-  scanf("%lf",&n);
-  return n;
+  int n = get_n();
+  complex c[n],result;
+  input_n_complex[c,n];
+  result= add_ncolplex(n,c);
+  out(n,c,result0;)
+  
 }
-float square_root(float n)
+int get_n()
 {
-  double sqrroot=sqrt(n);
-  return sqrroot;
+  int a;
+  printf("enter the number.\n");
+  scanf("%d", &a);
+  return(a);
+  
 }
-void output(float n ,float sqrroot)
+complex input_complexZ()
 {
-  printf("Square root of %.2lf =  %.2lf",n,sqrroot);
+  complex a;
+  printf("enter a and b where a + b is the first complex number.\n");
+  scanf("%d%d",&a.real,&a.img);
+  return(a);
 }
+void intput_n_complex(int n, comples c[n])
+{
+  int i;
+  for(i=0;i<n;i++)
+}
+  
