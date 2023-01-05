731c8cf2a012a01c8d65a48a8fd9b24a1cca68ef
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-01-05 09:58:18 +0000

    solved

--- set01/problem04.c
+++ set01/problem04.c
@@ -1,31 +1,47 @@
- , #include <stdio.h>
+#include,stdio.h>
 int input();
-void add(int a, int b, int *sum);
-void output(int a, int b, int sum);
+int compare(int a, int b, int c);
+void output(int a, int b, int c,int largest);
 
 int input()
 {
-  int n;
-  printf("enter the number:\n");
-  scanf("%d",&n);
+  int n; 
+  printf("Enter the number\n");
+  scanf("%d", &n);
   return n;
 }
-
-
-void add(int a, int b, int *sum)
+int compare(int a, int b, int c)
 {
-  *sum=a+b;
-}
-
-void output(int a, int b, int sum)
-{
-  printf("the sum of %d and %d is %d",a,b,sum);
-}
-int main()
-{
-  int a,b,sum;
-  a=input();
-  b=input();
-  add(a,b,&sum);
-  output(a,b,sum);
-}
+  if (a>b && a>c)
+  {
+    return a;
+  }
+  else if (b>a&& b>c)
+  {
+    return b;
+  }
+  
+    else if (c>a&&c>b)
+    {
+      return c;
+    }
+      
+  else
+    {
+      largest=c;
+    }
+void output(int a, int b, int c, int largedt)
+    {
+      printf("the largest of %d, %d and %d is %d\n",a,b,c,largest;)
+    }
+    int main()
+    {
+      int a,b,c,largest;
+      a=input();
+      b=input();
+      c=input();
+      largdwx3  est= compare(a,b,c);
+      output(a,b,c,largest);
+      return 0;
+    }
+    }
--- set01/problem05.c
+++ set01/problem05.c
@@ -2,49 +2,37 @@
 int input();
 int compare(int a, int b, int c);
 void output(int a, int b, int c, int largest);
-int main(){
-  int a, b, c, largest;
-  printf("Enter three no's : ");
-  a = input();
-  b = input();
-  c = input();
-  largest = compare(a, b, c);
-  output(a, b, c, largest);
+
+int input()
+{
+  int n;
+  printf("Enter a number\n");
+  scanf("%d", &n);
+  return n;
 }
-int input(){
-  int a;
-  scanf("%d", &a);
-  return a;
-  
-}
-int compare(int a, int b, int c){
-  if (a > b && a > c)
-  {
+int compare(int a, int b, int c)
+{
+  if (a > b && a > c){
    return a;
   }
-    else if (b > a && b > c){
+  else if (b > a && b > c){
     return b;
   }
   else if (c > a && c > b){
     return c;
   }
   else {
-    printf("invalid input");
+    largest=c;
   }
-}
 void output(int a, int b, int c, int largest){
-  if (largest == a){
-    printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
+printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
   }
-  else if(largest == b){
-    printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
-  }
-  else if (largest == c){
-    printf("The largest of %d, %d and %d is %d\n", a, b, c, largest);
-  }
-  else
-  {
-    printf("sorry, wrong input try again.\n");
-  }
-  
+int main(){
+  int a, b, c, largest;
+  a = input();
+  b = input();
+  c = input();
+  largest = compare(a, b, c);
+  output(a, b, c, largest);
+  return 0;
 }
