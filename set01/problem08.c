4f2edf4f8f4070152824ed66d18f8f251e1203ac
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-01-06 05:29:30 +0000

    solved

--- set01/problem08.c
+++ set01/problem08.c
@@ -1,56 +1,47 @@
-#include <stdio.h>
-
-
+#include<stdio.h>
 int input_array_size();
 void input_array(int n, int a[n]);
 int sum_n_array(int n, int a[n]);
-void output(int n, int a[n], int sum);
-
-
+void output(int n, int a[n],int sum);
 int input_array_size()
 {
   int n;
-  printf("Enter the size of the array\n");
-  scanf("%d", &n);
-  return n;
+  printf("enter array size:");
+  scanf("%d",&n);
+  return(n);
 }
-
 void input_array(int n, int a[n])
 {
-  for(int i=0;i<n;i++)
+  int i;
+  for(i=0;i<n;i++)
     {
-      printf("enter the element\n");
-      scanf("%d", &a[i]);
+      scanf("%d",&a[i]);
     }
 }
-
 int sum_n_array(int n, int a[n])
 {
-   int sum=0;
-  for(int i=0; i<n; i++)
+  int i,sum=0;
+  for(i=0;i<n;i++)
     {
       sum=sum+a[i];
     }
-  return sum;
+  return(sum);
 }
-
-void output(int n, int a[n], int sum)
+void output(int n, int a[n],int sum)
 {
-  printf("the sum of ");
-  for(int i=0;i<n;i++)
-    {
-    printf("%d ", a[i]);
-      }
-    printf("is %d", sum);
+  for (int i=0; i<n-1;i++){
+     printf("%d+",a[i]);
+  } 
+  printf("%d=%d",a[n-1],sum);
+ 
 }
-
 int main()
 {
-  int n, sum=0;
+  int n,sum;
   n=input_array_size();
   int a[n];
-  input_array(n, a);
-  sum=sum_n_array(n, a);
-  output(n, a,sum);
+  input_array(n,a);
+  sum=sum_n_array(n,a);
+  output(n, a ,sum);
   return 0;
 }
