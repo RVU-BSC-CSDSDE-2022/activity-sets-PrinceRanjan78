d85806ee89a08b8d3b467fd27dddbfb818bc96de
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-01-31 15:45:43 +0000

    solved

--- set01/problem05.c
+++ set01/problem05.c
@@ -1,42 +1,39 @@
-#include<stdio.h>
+#include <stdio.h>
 int input();
 int compare(int a, int b, int c);
-
-void output(int a, int b, int c, int largest);
-
+void output (int a, int b, int c, int largest);
 int input()
 {
-  int n;
-  printf("Enter a number\n");
-  scanf("%d", &n);
-  return n;
-  
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+return n;
 }
-int compare(int a, int b,int c)
+int compare(int a, int b, int c)
 {
-  if(a>b&&a>c){
-    return a;
-  }
-  else if (b>a &&b>c){
-    return b;
-  }
-  else if (c>a&&c>b){
-    return c;
-  }
-  else  
-  {
-    largest=c;
-  }
-  void output(int a, int b, int c, int largest){
-    printf("the largest of %d,%d and %d is %d\n",a,b,c,largest);
-  }
-int main(){
-  int a,b,c,largest;
-  a= input();
-  b=input();
-  c= input();
-  largest = compare(a,b,c);
-  output(a,b,c,largest);
-  return 0;
+int largest=a;
+
+if(b>largest);
+{
+largest=b;
 }
+if (c>largest)
+{
+largest=c;
+}
+return largest;
+}
+void output(int a, int b,int c, int largest)
+{
+printf("larger no of %d, %d and %d is %d",a,b,c,largest);
+}
+int main()
+
+{
+int a,b,c,largest;
+a=input();
+b=input();
+c=input();
+largest= compare(a,b,c);
+output(a,b,c,largest);
 }
