c37efc932ec8e89c903442189ba1ad78e874df0b
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-02-08 19:00:12 +0000

    solved

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,49 +1,54 @@
-#include<stdio.h>
+#include <stdio.h>
 int input_array_size();
-void input_array(int n, int a[n]);
-int sum_composite_numbers(int n, int a[n]);
+void input_array(int n,int a[n]);
+int sum_composite_numbers(int n,int a[n]);
 void output(int sum);
-int main()
+int input_array_size()
 {
-  int n, a[n], sum;
-  n = input_array_size();
-  input_array(n, &a[n]);
-  sum = sum_composite_numbers(n, &a[n]);
-  output(sum);
-}
-int input_array_size(){
-  int n;
-  printf("enter the size of the array: ");
-  if(scanf("%d", &n));
-  return n;
+int n;
+printf("enter the number:");
+scanf("%d",&n);
+printf("enter the element in an array:");
+return(n);
 }
 void input_array(int n, int a[n])
 {
-  int i;
-  printf("Enter the elements for the array: ");
-  for(i=0; i<n;i++){
-   if(scanf("%d", &a[i]));
-    }
-}
-int sum_composite_numbers(int n, int a[n])
+int i;
+for(i=0;i<n;i++)
 {
-  int i,isprime=0,sum=0;
-  for(i=1;i<n;i++)
-     {
-     for(int counter = 2; counter<= a[i]/2; counter++)
-     {
-        if((a[i]%counter) == 0)
-        {
-          isprime=1;}
-     }
-      if(isprime==1)
-      {
-        sum = sum+a[i];
-      } 
-     } 
-  return sum;
+scanf("%d",&a[i]);
+}
+}
+int sum_composite_number(int n,int a[n])
+{
+int i, result=0,var,sum=0,j;
+for(i=0;i<n;i++)
+{
+for(j=1;j<=a[i];j++)
+{
+if((a[i]%j)==0)
+{
+var++;
+}
+}
+if(var>2)
+{
+sum=sum+a[i];
+}
+}
+return sum;
 }
 void output(int sum)
 {
-  printf("sum is %d: \n",sum);
+printf("the sum of composite number is %d",sum);
+}
+int main()
+{
+int n,sum;
+n=input_array_size();
+int a[n];
+input_array(n,a);
+sum=sum_composite_number(n,a);
+output(sum);
+return 0;
 }
--- set02/problem07.c
+++ set02/problem07.c
@@ -1,39 +1,36 @@
-#include <stdio.h>
-
+#include<stdio.h>
 typedef struct triangle
 {
-   float base;
-   float altitude;
-   float area;
-} Triangle;
+float base;
+float altitude;
+float area;
+}
+Triangle;
 Triangle input_triangle();
 void find_area(Triangle *t);
 void output(Triangle t);
-
 int main()
 {
-   Triangle t;
-   t=input_triangle();
-   find_area(&t);
-   output(t);
-  return 0;
+Triangle t;
+t=input_triangle();
+find_area(&t);
+output(t);
+return 0;
 }
 Triangle input_triangle()
 {
-    Triangle t;
-    printf("Enter the base of the triangle\n");
-  scanf("%f" , &t.base);
-  printf("Enter the altitude of the triangle\n");
-  scanf("%f", &t.altitude);
-  return t;
+Triangle t;
+printf("enter the base of the triangle\n");
+scanf("%f", &t.base);
+printf("enter the altitude of the triangle \n");
+scanf("%f",&t.altitude);
+return t;
 }
-
 void find_area(Triangle *t)
 {
-      t->area=((t->base)*(t->altitude))*0.5;
+t->area=((t->base)*(t->altitude))*0.5;
 }
-
 void output(Triangle t)
 {
-   printf("The area of the triangle is %f", t.area);
+printf("the area of triangle with base %f and altitude %f is %f",t.base,t.altitude,t.area);
 }
