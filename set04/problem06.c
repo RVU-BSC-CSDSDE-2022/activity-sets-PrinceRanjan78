df3a2307045d6670b7bbeeb2f37ef14cee6bf460
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-02-16 18:52:37 +0000

    solved

--- set04/problem06.c
+++ set04/problem06.c
@@ -9,8 +9,8 @@ int main()
 {
 char string[100];
 input_string(string);
-int number_of _words;
-int number_of_words=count_words(string);
+int number_of_words;
+number_of_words=count_words(string);
 output(string,number_of_words);
 return 0;
 }
@@ -53,5 +53,4 @@ tok=strtok(NULL," ");
 }
 printf("' is %d\n",no_words);
 }
-
 
