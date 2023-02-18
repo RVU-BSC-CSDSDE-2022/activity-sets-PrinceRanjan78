da0ef2ed5caf3fc29defe784486d989d93e16c94
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-02-18 19:20:10 +0000

    solved

---
+++ set05/problem02.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<math.h>
+
+void input_camel_details(float *radius, float *height, float *length);
+float find_weight(float radius, float height, float length);
+
+void input_camel_details(float *radius, float *height, float *length)
+{
+float weight;
+printf("enter stomach radius of the camel\n");
+scanf("%f",radius);
+printf("enter height of the camel\n");
+scanf("%f",height);
+printf("enter length of the camel\n");
+scanf("%f",length);
+}
+
+float find_weight(float radius, float height, float length)
+{
+float pi=3.1415;
+float sqrt_hl;
+sqrt_hl=sqrt(height*length);
+return pi*(radius*radius*radius)*sqrt_hl;
+}
+
+void output(float radius, float height, float length, float weight)
+{
+print("the weight of the camel with stomach radius: %.2f, height : %.2f and length : %.2f is %.4f", radius, height, length, weight);
+}
+
+int main()
+{
+float radius, height, length;
+float weight;
+input_camel_details(&radius, &height, &length);
+weight=find_weight(radius,height,length);
+output(radius,height,length,weight);
+return 0;
+}
