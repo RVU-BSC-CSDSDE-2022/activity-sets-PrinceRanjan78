2221ee34eda23db0b4e12bd58967f9dfcb3cc40e
Prince  Ranjan <115694532+PrinceRanjan78@users.noreply.github.com>
2023-02-20 14:25:09 +0000

    solved

---
+++ set05/problem06.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+int input_n();
+void odd_average(int n, int a[n]);
+void output(float avg);
+
+int main()
+{
+int n;
+float avg;
+n=input_n();
+int a[n];
+input(n,a);
+avg=odd_average(n,a);
+output(avg);
+return 0;
+}
+
+int input_n()
+{
+int n;
+printf("enter the size of array\n");
+scanf("%d",&n);
+return n;
+}
+
+void input(int n, int a[n])
+{
+int i;
+for (i=0;i<n;i++)
+{
+printf("enter number %d of array\n",i+1);
+scanf("%d",&a[i]);
+}
+}
+
+float odd_average(int n,int a[n])
+{
+int i,sum=0,count=0;
+for (i=0;i<n;i++)
+{
+if (a[i]%2!=0)
+{
+sum=sum+a[i];
+count+=1;
+}
+}
+return sum/count;
+}
+
+void output(float avg)
+{
+printf("the average of the odd elements in the array is %.2f\n",avg);
+}
